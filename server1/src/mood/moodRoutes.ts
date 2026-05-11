2793725edd778482d886fe1fc98f0324d74563c7
Prashant Adhikari <student@prashantadhikari7.com.np>
2026-05-11 09:59:12 +0545

    clear

--- server1/src/mood/moodRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { logMood, getMoodHistory } from "./moodController.ts";
-
-const router = Router();
-
-router.post("/", logMood);
-router.get("/user/:userId", getMoodHistory);
-
-export default router;
